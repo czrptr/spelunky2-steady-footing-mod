ccbfffe210fa415e50f929eb5acc844f1b1e32b2
czrptr <cezar.c.petriuc@gmail.com>
2025-10-05 17:36:53 +0300

    Systems: Remove teetering player behavior

--- source/main.lua
+++ source/main.lua
@@ -5,3 +5,18 @@ meta = {
   description = "Never lose your balance, never drop your stuff",
   author = "Quasar",
 }
+
+local BEHAVIOR_TEETERING <const> = 0
+
+-- ==============================================================================
+
+local function handle_level_start()
+  for _, player in ipairs(get_local_players()) do
+    local behavior = player:get_base_behavior(BEHAVIOR_TEETERING)
+    player:clear_behavior(behavior)
+  end
+end
+
+-- ==============================================================================
+
+set_callback(handle_level_start, ON.LEVEL)
